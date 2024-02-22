ebc1a64fd98fe8e15fb136edd2db09a71a09598d
Sam Hanes <sam@maltera.com>
2024-02-22 04:38:04 -0800

    Use defmt for debug logging.

--- firmware/kernel/src/main.rs
+++ firmware/kernel/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
 
+use defmt;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
+
+use defmt_rtt as _;
 use panic_probe as _;
 
 #[embassy_executor::main]
@@ -11,6 +14,8 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     let mut led = Output::new(p.PC8, Level::High, Speed::Low);
+
+    defmt::info!("Coco-Chameleon starting up.");
 
     loop {
         led.set_high();
